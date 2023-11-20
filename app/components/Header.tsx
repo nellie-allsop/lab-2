bfbd1dff1112dce19023e751b68d7ac107c88472
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-20 13:37:35 +0000

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return (
+		<>
+			<h1 className="flex min-h-screen flex-col items-center justify-between p-24">
+				Telling you about me, using next.js
+			</h1>
+		</>
+	);
+}
