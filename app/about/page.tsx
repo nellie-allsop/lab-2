120750964ec07d50918c7f92547e1ab6f9681e9f
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-22 16:03:56 +0000

    add picture credit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,6 +11,7 @@ export default function AboutRoute() {
 		<main className="p-8 flex flex-col items-center justify-center">
 			<p className="p-8">This site was created by Nellie Allsop, an aspiring dev who currently lives in Norwich</p>
 			<Image className="rounded-full border-dashed border-8 border-blue-500" src="/Codebar.jpg" width={325} height={250} alt="Nellie being mentored at a codebar session" />
+			<p className="p-3"><strong>Picture credit:</strong> codebar Norwich</p>
 		</main>
 	);
 }
